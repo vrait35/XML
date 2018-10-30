7424a303dd232f6d336ac741f92efc4446c2d46f
Темирбулат Ануар Жанабекович <@corp.itstep>
2018-10-30 18:49:50 +0600

    xmlLesson

---
+++ XMLParcerLesson/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLParcerLesson
+{
+    public class Person
+    {
+        public string FullName { get; set; }
+        public DateTime? BirthDay { get; set; }
+        public int Age
+        {
+            get
+            {
+                if (BirthDay != null)
+                {
+                    return DateTime.Now.Year - BirthDay.Value.Year;
+                }
+                return 0;
+            }
+        }
+    }
+}
---
+++ XMLParcerLesson/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLParcerLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person> {
+                new Person
+                {
+                    FullName="Админ Админович Админов",
+                    BirthDay=DateTime.Now
+                },
+                new Person
+                {
+                    FullName="Админ2 Админович Админов",
+                    BirthDay=DateTime.Now
+                },
+                new Person
+                {
+                    FullName="Админ3 Админович Админов",
+                    BirthDay=DateTime.Now
+                },
+                new Person
+                {
+                    FullName="Админ4 Админович Админов",
+                    BirthDay=DateTime.Now
+                }
+            };
+            XmlDocument xml = new XmlDocument();
+            XmlNode rootElement = xml.CreateElement("People");
+            foreach(var person in people)
+            {
+                var personElement = xml.CreateElement("person");
+                personElement.SetAttribute("FullName", person.FullName);
+                personElement.SetAttribute("age", person.Age.ToString());
+                personElement.SetAttribute("birthDay", person.BirthDay.ToString());
+                rootElement.AppendChild(personElement);
+            }
+            xml.AppendChild(rootElement);
+            xml.Save("data.xml");
+
+            xml = new XmlDocument();
+            xml.Load("data.xml");
+
+            var root = xml.DocumentElement;
+            List<Person> loadedPeople = new List<Person>();
+
+            foreach( XmlNode element in root.ChildNodes)
+            {
+                Person person = null;
+                DateTime birthDate;
+
+              if(DateTime.TryParse(element.Attributes["birthDay"].Value,out birthDate))
+                {
+                    person = new Person
+                    {
+                        FullName = element.Attributes["FullName"].Value,
+                        BirthDay = birthDate
+                    };
+                }
+                else
+                {
+                    continue;
+                }
+                loadedPeople.Add(person);
+            }
+
+
+
+//
+//            XmlWriterSettings settings = new XmlWriterSettings();
+//            settings.Encoding = Encoding.UTF8;
+//            XmlWriter xmlWriter = XmlWriter.Create("data.xml", settings);
+        }
+    }
+}
